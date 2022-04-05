3bff5584237326a37685ac7e505f2b0c8ca5883c
jsteinberg7 <j.steinberg702@gmail.com>
2022-04-05 18:09:50 -0400

    prevented popup overflow

--- lib/services/dynamicLinks.dart
+++ lib/services/dynamicLinks.dart
@@ -6,19 +6,21 @@ import '../constants/popups.dart';
 abstract class DynamicLinkService {
   static Future<void> retrieveDynamicLink(BuildContext context) async {
     try {
-      final PendingDynamicLinkData data =
-          await FirebaseDynamicLinks.instance.getInitialLink();
-      final Uri deepLink = data?.link;
-      // print("RECEIVING DEEP LINK: " + deepLink?.toString());
-      if (deepLink != null) {
-        Popups.showUserProfilePopupNew(context,
-            friendSoshiUsername: "jason", refreshScreen: () {});
+      if (!Popups.popup_live) {
+        final PendingDynamicLinkData data =
+            await FirebaseDynamicLinks.instance.getInitialLink();
+        final Uri deepLink = data?.link;
+        print("RECEIVING DEEP LINK: " + deepLink?.toString());
+        if (deepLink != null) {
+          await Popups.showUserProfilePopupNew(context,
+              friendSoshiUsername: "jason", refreshScreen: () {});
+        }
+        FirebaseDynamicLinks.instance.onLink
+            .listen((PendingDynamicLinkData dynamicLink) async {
+          await Popups.showUserProfilePopupNew(context,
+              friendSoshiUsername: "jason", refreshScreen: () {});
+        });
       }
-      FirebaseDynamicLinks.instance.onLink
-          .listen((PendingDynamicLinkData dynamicLink) async {
-        Popups.showUserProfilePopupNew(context,
-            friendSoshiUsername: "jason", refreshScreen: () {});
-      });
     } catch (e) {
       print(e.toString());
     }
